f9bc69523873c6b9f534b233428b045ddf73b0b7
Ilya Voronin <voroninilya115@gmail.com>
2019-04-06 18:27:51 +0300

    Add ConstantFolder implementation

--- task06/folder.py
+++ task06/folder.py
@@ -3,31 +3,56 @@ from model import *
 
 class ConstantFolder():
     def visit_number(self, number):
-        pass
+        return Number(number)
 
     def visit_function(self, function):
-        pass
+        return Function(function.args,
+                        [stmt.accept(self) for stmt in function.body])
 
     def visit_function_definition(self, function_definition):
-        pass
+        return FunctionDefinition(function_definition.name,
+                                  function_definition.function.accept(self))
 
     def visit_conditional(self, conditional):
-        pass
+        return Conditional(
+            conditional.condition.accept(self),
+            [stmt.accept(self) for stmt in conditional.if_true],
+            [stmt.accept(self)for stmt in conditional.if_false])
 
     def visit_print(self, print_):
-        pass
+        return Print(print_.expr.accept(self))
 
     def visit_read(self, read):
-        pass
+        return Read(read.name)
 
     def visit_function_call(self, function_call):
-        pass
+        return FunctionCall(
+               function_call.fun_expr.accept(self),
+               [stmt.accept(self) for stmt in function_call.args])
 
     def visit_reference(self, reference):
-        pass
+        return Reference(reference.name)
 
     def visit_binary_operation(self, binary_operation):
-        pass
+        lhs = binary_operation.lhs
+        op = binary_operation.op
+        rhs = binary_operation.rhs
+        s = Scope()
+        if isinstance(lhs, Number) and isinstance(rhs, Number):
+            return binary_operation.evaluate(s)
+        if (isinstance(lhs, Number) and lhs.value == 0 and
+                isinstance(rhs, Reference)):
+            return Number(0)
+        if (isinstance(rhs, Number) and rhs.value == 0 and
+                isinstance(lhs, Reference)):
+            return Number(0)
+        if (isinstance(lhs, Reference) and isinstance(rhs, Reference) and
+                lhs.name == rhs.name and op == '-'):
+            return Number(0)
 
     def visit_unary_operation(self, unary_operation):
-        pass
+        expr = unary_operation.expr
+        op = unary_operation.op
+        s = Scope()
+        if isinstance(expr, Number):
+            return unary_operation.evaluate(s)
