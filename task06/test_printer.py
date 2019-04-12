ae8cd9d676f1af160c036b6c37b1d0fb5e43b324
Ilya Voronin <voroninilya115@gmail.com>
2019-04-12 14:30:20 +0300

    Fix codestyle in printer tests

--- task06/test_printer.py
+++ task06/test_printer.py
@@ -1,12 +1,13 @@
 import pytest
 from printer import *
+import textwrap
 
 
 def test_visit_number():
     printer = PrettyPrinter()
     num = Number(101)
     num.accept(printer)
-    assert printer.out == '101'
+    assert printer.get_out() == '101;\n'
 
 
 def test_visit_conditional():
@@ -14,16 +15,19 @@ def test_visit_conditional():
     cond1 = Conditional(Number(1), [Number(10), Number(11)])
     cond2 = Conditional(Number(42), [cond1, Number(12)], [Number(13)])
     cond2.accept(printer)
-    assert printer.out == \
-        'if (42) {\n' + \
-        '    if (1) {\n' + \
-        '        10;\n' + \
-        '        11;\n' + \
-        '    }\n' + \
-        '    12;\n' + \
-        '} else {\n' + \
-        '    13;\n' + \
-        '}'
+    assert printer.get_out() == textwrap.dedent(
+        '''\
+        if (42) {
+            if (1) {
+                10;
+                11;
+            }
+            12;
+        } else {
+            13;
+        }
+        '''
+    )
 
 
 def test_visit_function_definition():
@@ -31,32 +35,35 @@ def test_visit_function_definition():
     func = Function(['a', 'b'], [Number(12), Number(13)])
     func_def = FunctionDefinition('func', func)
     func_def.accept(printer)
-    assert printer.out == \
-        'def func(a, b) {\n' + \
-        '    12;\n' + \
-        '    13;\n' + \
-        '}'
+    assert printer.get_out() == textwrap.dedent(
+        '''\
+        def func(a, b) {
+            12;
+            13;
+        }
+        '''
+    )
 
 
 def test_visit_print():
     printer = PrettyPrinter()
     print_ = Print(Number(42))
     print_.accept(printer)
-    assert printer.out == 'print 42'
+    assert printer.get_out() == 'print 42;\n'
 
 
 def test_visit_read():
     printer = PrettyPrinter()
     read = Read("aaa")
     read.accept(printer)
-    assert printer.out == 'read aaa'
+    assert printer.get_out() == 'read aaa;\n'
 
 
 def test_visit_reference():
     printer = PrettyPrinter()
     ref = Reference('aaa')
     ref.accept(printer)
-    assert printer.out == 'aaa'
+    assert printer.get_out() == 'aaa;\n'
 
 
 def test_unary_operation():
@@ -80,7 +87,7 @@ def test_function_call():
     func_call1 = FunctionCall(Reference('f1'), [Number(10), Number(11)])
     func_call2 = FunctionCall(Reference('f2'), [Number(12), func_call1])
     func_call2.accept(printer)
-    assert printer.out == 'f2(12, f1(10, 11))'
+    assert printer.get_out() == 'f2(12, f1(10, 11));\n'
 
 
 def test_all(capsys):
@@ -101,17 +108,20 @@ def test_all(capsys):
     ])))
     out = capsys.readouterr().out
     print(out)
-    assert out == \
-        'def main(arg1) {\n' + \
-        '    read x;\n' + \
-        '    print x;\n' + \
-        '    if ((2) == (3)) {\n' + \
-        '        if (1) {\n' + \
-        '        }\n' + \
-        '    } else {\n' + \
-        '        exit(-arg1);\n' + \
-        '    }\n' + \
-        '}\n'
+    assert out == textwrap.dedent(
+        '''\
+        def main(arg1) {
+            read x;
+            print x;
+            if ((2) == (3)) {
+                if (1) {
+                }
+            } else {
+                exit(-arg1);
+            }
+        }
+        '''
+    )
 
 
 if __name__ == "__main__":
