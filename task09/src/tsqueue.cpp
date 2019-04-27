63524ddfae1f68f9e5992b9c31abbe5fdb8e74f5
Ilya Voronin <voroninilya115@gmail.com>
2019-04-27 15:27:08 +0300

    fix codestyle

--- task09/src/tsqueue.cpp
+++ task09/src/tsqueue.cpp
@@ -2,27 +2,27 @@
 
 void threadsafe_queue_init(ThreadsafeQueue *q) {
     pthread_mutex_init(&q->m, NULL);
-    pthread_cond_init(&q->cond_is_empty, NULL);
+    pthread_cond_init(&q->cond_is_not_empty, NULL);
     queue_init(&(q->q));
 }
 
 void threadsafe_queue_destroy(ThreadsafeQueue *q) {
     queue_destroy(&q->q);
-    pthread_cond_destroy(&q->cond_is_empty);
+    pthread_cond_destroy(&q->cond_is_not_empty);
     pthread_mutex_destroy(&q->m);
 }
 
 void threadsafe_queue_push(ThreadsafeQueue *q, void *data) {
     pthread_mutex_lock(&q->m);
     queue_push(&q->q, data);
-    pthread_cond_signal(&q->cond_is_empty);
+    pthread_cond_signal(&q->cond_is_not_empty);
     pthread_mutex_unlock(&q->m);
 }
 
 void *threadsafe_queue_wait_and_pop(ThreadsafeQueue *q) {
     pthread_mutex_lock(&q->m);
     while (queue_empty(&q->q)) {
-        pthread_cond_wait(&q->cond_is_empty, &q->m);
+        pthread_cond_wait(&q->cond_is_not_empty, &q->m);
     }
     void *first_elem = queue_pop(&q->q);
     pthread_mutex_unlock(&q->m);
