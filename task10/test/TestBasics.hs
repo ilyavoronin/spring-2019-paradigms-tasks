5c04430021d5c37cc2a867ebd9885ad62e8d580c
Ilya Voronin <voroninilya115@gmail.com>
2019-05-08 14:38:17 +0300

    add tests

--- task10/test/TestBasics.hs
+++ task10/test/TestBasics.hs
@@ -11,17 +11,32 @@ testsBasics = testGroup "Unit tests for Basics tasks"
     [testCase "head' works on non-empty list" $
         head' [1,2,3] @?= 1
 
+    , testCase "head' works on infinite list" $
+        head' [1,2..] @?= 1
+
     , testCase "tail' works on non-empty list too" $
         tail' [1,2,3] @?= [2,3]
+
+    , testCase "tail' works on infinite list too" $
+        take' 10 (tail' [1,2..]) @?= take' 100 [2..]
 
     , testCase "take' takes 1 element from 3-element list" $
         take' 1 [1,2,3] @?= [1]
 
+    , testCase "take' takes 3 element from infinite list" $
+        take' 3 [1,2..] @?= [1,2,3]
+
     , testCase "drop' drops 1 element from 3-element list" $
         drop' 1 [1,2,3] @?= [2,3]
 
+    , testCase "drop' drops 3 element from infinite list" $
+        take' 10 (drop' 3 [1,2..]) @?= take' 10 [4..]
+
     , testCase "filter' selects only even numbers from 0 to 10" $
         filter' even [0..10] @?= [0,2..10]
+
+    , testCase "filter' selects only even numbers from infinite list" $
+        take' 10 (filter' even [0..]) @?= take' 10 [0,2..]
 
     , testCase "foldl'' can be used for finding sum of elements" $
         foldl'' (+) 0 [1,2,3] @?= 6
--- task10/test/TestRobots.hs
+++ task10/test/TestRobots.hs
@@ -9,9 +9,31 @@ main = defaultMain testsRobots
 testsRobots :: TestTree
 testsRobots = let
         walter = robot "Walter" 50 50
+        peter  = robot "Peter" 25 100
+        dead   = robot "dead" 10000 0
     in testGroup "Unit tests for Robots task"
         [ testCase "Test for getName" $
             getName walter @?= "Walter"
+        , testCase "Test for getAttack" $
+            getAttack walter @?= 50
+        , testCase "Test for getHealth" $
+            getHealth walter @?= 50
+        , testCase "Test for setName" $
+            setName "Tom" walter @?= robot "Tom" 50 50
+        , testCase "Test for setAttack" $
+            setAttack 100 walter @?= robot "Walter" 100 50
+        , testCase "Test for setHealth" $
+            setHealth 100 walter @?= robot "Walter" 50 100
         , testCase "Test for printRobot" $
             printRobot walter @?= "Walter, attack: 50, health: 50"
+        , testCase "Test for damage" $
+            damage walter 30 @?= robot "Walter" 50 20
+        , testCase "Test for isAlive" $
+            isAlive dead @?= False
+        , testCase "Test for fight" $
+            fight walter peter @?= robot "Peter" 25 50
+        , testCase "Test for threeRoundFight" $
+            threeRoundFight walter peter @?= robot "Walter" 50 25
+        , testCase "Test for survivors" $
+            survivors @?= [("a",100,600)]
         ]
